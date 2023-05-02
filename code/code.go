d75e9fc245cf461410d23d5bbfc9b6ce5740b01e
shypang <smartdragon417@gmail.com>
2023-05-02 23:30:19 +0900

    Compiler 만들기 실습, 테스트 코드 작성

--- code/code.go
+++ code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 //바이트 코드는 명령어로 되어있다.
 //명령어는 바이트 열이고,  명령어 하나는 명령코드 하나와 피연산자를 0개 이상 가진다.
@@ -14,9 +17,10 @@ const (
 	OpConstant Opcode = iota //피연산자를 하나 가진다.
 )
 
+// op 코드의 정의는 필드 Name과 OperandWidths를 갖는다.
 type Definition struct {
-	Name          string
-	OperandWidths []int
+	Name          string //Opcode를 사람이 읽을 수 있는 이름으로 담는다.
+	OperandWidths []int  //OperandWidth는 각각의 피연산자가 차지하는 바이트의 크기를 담는다.
 }
 
 var definitions = map[Opcode]*Definition{
@@ -29,4 +33,32 @@ func Lookup(op byte) (*Definition, error) {
 		return nil, fmt.Errorf("opcode %d undefined", op)
 	}
 	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+
+		}
+		offset += width
+
+	}
+	return instruction
 }
